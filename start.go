cfc16ae781dff4e2892076623bc41f8544f20f70
윤지수 <yoonjs741@gmail.com>
2020-09-26 20:07:37 +0900

    Factorial function from 1 to 10.

--- start.go
+++ start.go
@@ -3,82 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	println("Hello world!")
-	var a, c int
-	a = 3
-	b := 5
-	c = a + b
-	d := "Bye world.."
-	var k int = 3
-	const sangsu = "Punto"
-	const (
-		sangsu1 = iota
-		sangsu2
-		sangsu3
-	)
-
-	msg := "IBI"
-	saysomething("Hi", "I", "am", "Punto")
-
-	println(c, d, sangsu3)
-
-	if k == 1 {
-		println("One")
-	} else if k == 2 {
-		println("Two")
-	} else {
-		println("Other")
+	println("START")
+	var a = 10
+	for i := 1; i <= a; i++ {
+		sum(i)
 	}
-
-	var name string
-	var category = 3
-	x := category << 1
-	switch x {
-	case 1:
-		name = "Punto"
-	case 2:
-		name = "DonJon"
-	case 3, 4:
-		name = "Ramge"
-	default:
-		name = "Asson"
-	}
-
-	sum := 0
-	for i := 1; i <= 10; i++ {
-		sum += i
-	}
-	println(name, x)
-	var inpNum = x
-	fmt.Println(dec2bin(inpNum))
-	println(sum)
-	say(&msg)
-	println(msg)
+	println("END")
 }
 
-func saysomething(txt ...string) {
-	for _, s := range txt {
-		println(s)
+func sum(a int) {
+	var factorial int = 1
+	//
+	// a의 팩토리얼 구하는 함수 자리
+	//
+	for i := 1; i <= a; i++ {
+		factorial *= i
 	}
-}
-
-func say(msg *string) {
-	println(*msg)
-	*msg = "Changed"
-}
-
-func dec2bin(dec int) string {
-	bin := ""
-	if dec >= 1 {
-		bin = fmt.Sprintf("%d", dec%2) + bin
-		bin = dec2bin(dec/2) + bin
-	}
-	return bin
-}
-
-func calc() {
-	k := 1
-	if k == 1 {
-		println("One")
-	}
+	result := fmt.Sprintf("%d! = %d", a, factorial)
+	println(result)
 }
